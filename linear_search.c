91c87bfd9a7816e267599d37730f1938962fdb64
Mayank-Joshi-abc <78486342+Mayank-Joshi-abc@users.noreply.github.com>
2021-07-14 15:39:22 +0530

    Update linear_search.c

--- linear_search.c
+++ linear_search.c
@@ -1,29 +1,34 @@
 #include<stdio.h>
 int main()
 {
-    int ar[100],n,f=0,i,ele;
+    int ar[100],n,f=-1,i,ele;
     printf("Enter no. of elements: ");
     scanf("%d",&n);
-    printf("Enter element to search: ");
+    printf("\nEnter element to search: ");
     scanf("%d",&ele);
+    printf("\nEnter the elements\n");
     for(i = 0;i<n;i++)
     {
         scanf("%d",&ar[i]);
+    }
+    for(i = 0;i<n;i++)
+    {
         if(ar[i] == ele)
         {
-            f = 1;
+            printf("break");
+            f = i;
             break;
         }
     }
-    if(f = 0)
+    if(f == -1)
     {
         printf("\nElement not found");
-        printf("\nNo. of comparisons: ",i+1);
+        printf("\nNo. of comparisons: %d",n);
     }
     else
     {
-        printf("\nElement found at index :",i);
-        printf("\nNo. of comparisons: ",i+1);
+        printf("\nElement found at index : %d",f);
+        printf("\nNo. of comparisons: %d",f+1);
     }
     return 0;
 }
